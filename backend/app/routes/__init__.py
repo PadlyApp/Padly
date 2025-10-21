e2dd3fdd11d0f6f57d0f3290b932a50e38e4c21a
amaan <amaan.farooqi@mail.utoronto.ca>
2025-10-21 01:55:39 -0400

    Added jwt auth to the backend with signup and login

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -6,9 +6,11 @@ API endpoint definitions
 from .users import router as users_router
 from .listings import router as listings_router
 from .admin import router as admin_router
+from .auth import router as auth_router
 
 __all__ = [
     "users_router",
-    "listings_router",
+    "listings_router", 
     "admin_router",
+    "auth_router"
 ]
